5c203e422df93de68deb393c69658e7d184a3424
dupengfei <3064717379@qq.com>
2021-09-06 16:59:29 +0800

    API中建立dto存放实体类

--- huiminpay_merchant/huiminpay_merchant_api/src/main/java/com/huiminpay/dto/MerchantDto.java
+++ huiminpay_merchant/huiminpay_merchant_api/src/main/java/com/huiminpay/dto/MerchantDto.java
@@ -1,7 +1,5 @@
-package com.huiminpay.api.dto;
+package com.huiminpay.dto;
 
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
